75e82f5c75a78e4fed42f17852b045c69c256a21
Jeang <2609231566@qq.com>
2026-05-11 23:51:47 +0800

    商品列表渲染

---
+++ 学习/案例/07 商品列表/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="app">
+    <table>
+      <thead>
+        <tr>
+          <th>编号</th>
+          <th>商品</th>
+          <th>名称</th>
+          <th>备注</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in list" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>
+            <img :src="item.src" :title="item.name" />
+          </td>
+          <td style="font-size: 12px;">{{item.name}}</td>
+          <td>
+            <MyTag v-model="item.log"></MyTag>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import MyTag from "./components/My-tag.vue";
+export default {
+  components: {
+    MyTag,
+  },
+  data() {
+    return {
+      inpShow: false,
+      list: [
+        {
+          id: 101,
+          src: "https://img11.360buyimg.com/jdcms/s240x240_jfs/t1/400538/34/14533/62777/69ae68ecFfe01dfbe/008332032015fe60.jpg.avif",
+          name: "中国黄金(CHINA GOLD)阴山翠玉髓手镯女和田玉镯子",
+          log: "镯子",
+        },
+        {
+          id: 102,
+          src: "https://img30.360buyimg.com/jdcms/s240x240_jfs/t1/412205/25/5551/296951/69cb6446Fa10a6e18/0083320320320517.png.avif",
+          name: "苹果16pro iPhone16ProMax移动联通电信全网通5G手机",
+          log: "手机",
+        },
+        {
+          id: 103,
+          src: "https://img10.360buyimg.com/pcpubliccms/s1440x1440_jfs/t1/428558/13/16266/1787161/69fdae24F044e40f5/0083000000b0cc73.jpg.avif",
+          name: "阿迪达斯(adidas)短袖t恤男士夏季跑步健身运动衣快干衣透气男装T恤 黑白 L",
+          log: "衣服",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+table {
+  margin: 50px auto;
+  width: 80%;
+  border-collapse: collapse;
+  //tr变化时不影响表格整体
+  table-layout: fixed;
+}
+th,
+td {
+  height: 35px;
+  padding: 5px;
+  text-align: center;
+  overflow: hidden;
+  img {
+    width: 80px;
+    height: 80px;
+    object-fit: cover;
+    display: block;
+    margin: 0 auto;
+  }
+}
+th {
+  background-color: #f7f7f7;
+  border-bottom: 2px solid #006bd2;
+}
+td {
+  border-bottom: 1px solid #ccc;
+}
+</style>
---
+++ 学习/案例/07 商品列表/components/My-tag.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <input
+      v-if="inpShow"
+      type="text"
+      value=""
+      v-focus
+      @blur="inpShow = false"
+    />
+    <span v-else style="cursor: pointer" @dblclick="pushInp">xxxx</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inpShow: false,
+    };
+  },
+  methods: {
+    pushInp() {
+      setTimeout(() => {
+        this.inpShow = true;
+        // 通过$nextTick聚焦
+        // this.$nextTick(() => {
+        //   this.$refs.inp.focus();
+        // });
+        this.def=this.inp
+      }, 500);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+input {
+  padding: 0 8px;
+  width: 70px;
+  height: 20px;
+  border: 1px solid #006bd2;
+  border-radius: 4px;
+  outline: none;
+}
+</style>
