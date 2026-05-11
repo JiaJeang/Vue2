ea204ba010980109bf7b6e99961da093623172a9
Jeang <2609231566@qq.com>
2026-05-11 14:19:13 +0800

    作用域插槽

---
+++ 学习/组件化/12 作用域插槽/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <BaseTab :list="list1">
+      <!-- 
+        默认插槽 #default="obj"
+        具名插槽 #插槽名='变量名' 接收插槽传递过来的数据(对象)
+      -->
+      <template #default="obj">
+        <button :style="{'background-color': color1}" @click="del(obj.item.id)">
+          删除
+        </button>
+      </template>
+    </BaseTab>
+    <BaseTab :list="list2">
+      <!-- 
+        也可以直接解构赋值接收插槽传递过来的数据(对象)
+       -->
+      <template #default="{ item }">
+        <button :style="{'background-color': color2}" @click="look(item)">
+          查看
+        </button>
+      </template>
+    </BaseTab>
+  </div>
+</template>
+
+<script>
+import BaseTab from "./components/BaseTab.vue";
+export default {
+  components: {
+    BaseTab,
+  },
+  data() {
+    return {
+      color1: "red",
+      color2: "#4eac00",
+      list1: [
+        { id: 1, name: "张三", age: 18, sex: "男" },
+        { id: 2, name: "李四", age: 19, sex: "女" },
+        { id: 3, name: "王五", age: 20, sex: "男" },
+      ],
+      list2: [
+        { id: 1, name: "张三", age: 18, sex: "男" },
+        { id: 2, name: "李四", age: 19, sex: "女" },
+        { id: 3, name: "王五", age: 20, sex: "男" },
+      ],
+    };
+  },
+  methods: {
+    del(id) {
+      this.list1 = this.list1.filter((item) => item.id !== id);
+    },
+    look(item) {
+      alert(`姓名:${item.name},年龄:${item.age},性别:${item.sex}`);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 学习/组件化/12 作用域插槽/components/BaseTab.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="tab">
+    <table>
+      <thead>
+        <tr>
+          <th>序号</th>
+          <th>姓名</th>
+          <th>年龄</th>
+          <th>性别</th>
+          <th>操作</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, ind) in list" :key="ind.id">
+          <td>{{ ind + 1 }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.age }}</td>
+          <td>{{ item.sex }}</td>
+          <td>
+            <slot :item="item" smg="传递成功"></slot>
+            <!-- 
+                传递的为一个对象:
+                {
+                    item:传递过来的数组元素,
+                    smg:"传递成功"
+                }
+            -->
+          </td>
+        </tr>
+      </tbody>
+      <tfoot></tfoot>
+    </table>
+    <br />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    list: Array,
+  },
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+table {
+  width: 300px;
+  border-spacing: 0;
+}
+th {
+  background-color: #4eace7;
+}
+table,
+td,
+th {
+  border: 1px solid #000;
+  height: 38px;
+  text-align: center;
+}
+button {
+  background-color: red;
+  border: 0;
+  width: 40px;
+  height: 25px;
+  border-radius: 5px;
+}
+</style>
