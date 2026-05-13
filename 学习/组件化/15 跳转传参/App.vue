7ca7d5aaac69d09e13e120f5c5fb73ace5e50243
Jeang <2609231566@qq.com>
2026-05-13 14:45:55 +0800

    跳转传参

---
+++ 学习/组件化/15 跳转传参/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <div class="head">
+      <router-link to="/home">首页</router-link>
+      <router-link to="/search">搜索</router-link>
+    </div>
+    <div class="main">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+html {
+  background-color: #E7DFDD;
+}
+</style>
+
+<style lang="less" scoped>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.head {
+  height:40px;
+  background-color: #fff;
+  border-radius: 15px;
+  display: flex;
+  a {
+    text-decoration: none;
+    text-align: center;
+    color: white;
+    line-height: 40px;
+    width: 80px;
+    background-color: #b83624;
+    margin-left: 10px;
+    border-radius: 10px;
+  }
+}
+.main {
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+}
+</style>
---
+++ 学习/组件化/15 跳转传参/views/Home.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="home">
+    <div class="head">
+      <img src="@/utils/cat.gif" alt="" />
+    </div>
+    <div class="inp">
+      <input type="text" placeholder="" />
+      <button>搜索🔍</button>
+    </div>
+    <div class="hot">
+        热门搜索:
+        <!-- 
+            查询参数传参 用于传递多个参数(&连接)的场景 
+            跳转: to="/path?参数名=值"
+            接收: $toute.query.参数名
+        -->
+        <!-- <router-link to="/search?key=demo1">demo1</router-link> -->
+        <!-- 
+            动态路由传参 传单个参数方便 
+            路由: /path/:参数名
+            跳转: to="/path/值" 
+            接收: $toute.params.参数名
+        -->
+        <router-link to="/search/demo2">demo2</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyHome",
+};
+</script>
+
+<style lang="less" scoped>
+.head {
+  display: flex;
+  justify-content: center;
+  img {
+    width: auto;
+    height: 100px;
+  }
+}
+.inp {
+  margin: 20px auto 0;
+  height: 30px;
+  width: 500px;
+  background-color: pink;
+  display: flex;
+  border-radius: 10px;
+  overflow: hidden;
+  input {
+    flex: 1;
+    border: 0px;
+    outline: none;
+    padding-left: 10px;
+    font-size: 18px;
+  }
+  button {
+    width: 80px;
+    border: 0px;
+  }
+}
+.hot{
+    width: 500px;
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 20px;
+    a{
+        margin-left: 10px;
+    }
+}
+</style>
